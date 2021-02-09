9b862ec2b4593a38bf8a87d18551f7ff0fd644fe
Rubens Pereira <rubenscp@gmail.com>
2021-02-08 21:51:39 -0300

    Inclusão das classes entidade

---
+++ Entity/DetectedObject.py
@@ -0,0 +1,28 @@
+# Detected Object Class
+
+
+class DetectedObject:
+
+    def getValueOf(className):
+        if className == 'exuvia':
+            return 0
+
+        if className == 'instar1':
+            return 1
+
+        if className == 'instar2':
+            return 2
+
+        if className == 'instar3':
+            return 3
+
+        if className == 'instar4':
+            return 4
+
+        if className == 'adulta':
+            return 5
+
+        if className == 'ovo':
+            return 6
+
+        return 99
